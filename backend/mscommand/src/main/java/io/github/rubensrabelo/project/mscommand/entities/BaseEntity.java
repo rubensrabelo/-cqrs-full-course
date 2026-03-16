4c99a22180f1c876a231a550e85e2bab27266a98
Rubens Rabelo <rubensrabelo123@gmail.com>
2026-03-16 19:05:51 -0300

    feat(backend-mscommand): add entities (wip)

---
+++ backend/mscommand/src/main/java/io/github/rubensrabelo/project/mscommand/entities/BaseEntity.java
@@ -0,0 +1,77 @@
+package io.github.rubensrabelo.project.mscommand.entities;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @CreationTimestamp()
+    @Column()
+    private LocalDateTime createdAt;
+    
+    @UpdateTimestamp()
+    @Column()
+    private LocalDateTime updatedAt;
+
+    public BaseEntity() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        BaseEntity other = (BaseEntity) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+}
---
+++ backend/mscommand/src/main/java/io/github/rubensrabelo/project/mscommand/entities/CustomerEntity.java
@@ -0,0 +1,91 @@
+package io.github.rubensrabelo.project.mscommand.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "customers")
+public class CustomerEntity {
+
+    @Column(nullable = false, length = 100)
+    private String name;
+    
+    @Column(nullable = false, length = 100)
+    private String email;
+    
+    @Column(nullable = false, length = 100)
+    private String phone;
+
+    // @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
+    // private List<AppointmentsEntity> appointments;
+
+    public CustomerEntity() {
+    }
+
+    public CustomerEntity(Builder builder) {
+        this.name =  builder.name;
+        this.email = builder.email;
+        this.phone = builder.phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public static class Builder {
+
+        private String name;
+        private String email;
+        private String phone;
+        // private List<AppointmentsEntity> appointments
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder phone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        // public Builder appointments(List<AppointmentsEntity> appointments) {
+           // this.appointments = appointments;
+           // return this;
+        // }
+
+        public CustomerEntity build() {
+            return new CustomerEntity(this);
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+}
