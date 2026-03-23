8d3e8c409e45ebd11461e927e415713d1b69e0ba
Rubens Rabelo <rubensrabelo123@gmail.com>
2026-03-23 14:20:21 -0300

    feat(backend-mssync): add dtos

---
+++ backend/mssync/src/main/java/io/githuib/rubensrabelo/project/mssync/MssyncApplication.java
@@ -0,0 +1,13 @@
+package io.githuib.rubensrabelo.project.mssync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MssyncApplication.class, args);
+	}
+
+}
---
+++ backend/mssync/src/main/java/io/githuib/rubensrabelo/project/mssync/dtos/customers/CustomerDTO.java
@@ -0,0 +1,88 @@
+package io.githuib.rubensrabelo.project.mssync.dtos.customers;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collation = "customers")
+public class CustomerDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+
+    public CustomerDTO() {
+    }
+
+    private CustomerDTO(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.email = builder.email;
+        this.phone = builder.phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public static class Builder {
+        private Long id;
+        private String name;
+        private String email;
+        private String phone;
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder phone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public CustomerDTO build() {
+            return new CustomerDTO(this);
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+}
