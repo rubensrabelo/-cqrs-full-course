e1a02d0357cb41d1bf22fe99bfb24a298ae42ca7
Rubens Rabelo <rubensrabelo123@gmail.com>
2026-03-24 09:36:22 -0300

    feat(backend-msquery): start project

---
+++ backend/msquery/src/test/java/io/github/rubensrabelo/msquery/MsqueryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rubensrabelo.msquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsqueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
